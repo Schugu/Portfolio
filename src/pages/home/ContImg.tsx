a1816a324c8a971475d7675b19b1c8a5f442b266
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-04 15:48:11 -0300

    Add: Variables de color

--- src/pages/home/ContImg.tsx
+++ src/pages/home/ContImg.tsx
@@ -8,7 +8,7 @@ export default function ContImg({ children, shape }: contImgProps) {
   return (
     <div className={`w-48 h-48 md:min-w-80 md:h-80 xl:min-w-96 xl:h-96 
       ${shape === "circle" ? "rounded-full overflow-hidden" : "rounded"}
-    border-2 border-solid border-AZUL dark:border-AMARILLO bg-gradient-to-b  from-AZUL to-AMARILLO`}>
+    border-2 border-[var(--color-primary)] bg-gradient-to-b  from-[var(--color-secondary)] to-[var(--color-primary)]`}>
       {children}
     </div>
   )
