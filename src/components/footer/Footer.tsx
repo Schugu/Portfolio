e88d2b3f0240bd14bf72bda5351cacaa7b03e508
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-05 09:15:48 -0300

    Add: Variables de color

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,17 +6,17 @@ export default function Footer() {
 
   return (
     <footer className="w-full min-h-10 p-2 gap-2 flex flex-wrap items-center justify-center
-    bg-LIGHT-BG-dark dark:bg-DARK-BG-dark dark:text-CREMA text-LIGHT-AzulOscuro">
+    bg-[var(--color-background)] text-[var(--color-text)]">
       <p className="text-center">{t("global:footer.template")} Leandro Daniel Schugurensky</p>
       <Tooltip text='Github'>
         <a href="https://github.com/Schugu" target="_blank"
-          className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
+          className="flex justify-center items-center hover:text-[var(--color-primary)]">
           <span className="w-5 h-5 icon-[mdi--github]" />
         </a>
       </Tooltip>
       <Tooltip text='Linkedin'>
         <a href="https://www.linkedin.com/in/leandrodanielschugurensky/" target="_blank"
-          className="flex justify-center items-center hover:text-AZUL dark:hover:text-AMARILLO">
+          className="flex justify-center items-center hover:text-[var(--color-primary)]">
           <span className="w-5 h-5 icon-[mdi--linkedin]" />
         </a>
       </Tooltip>
