3f190adfebafa6a8249d8bb37506752db229382b
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-08 17:25:16 -0300

    Add: Lógica dentro del componente para ahorrar código

--- src/components/ImgModal.tsx
+++ src/components/ImgModal.tsx
@@ -1,10 +1,39 @@
-import React, { useEffect } from 'react';
+import { useState, useEffect, MouseEvent, ReactNode } from 'react';
 
-const ImgModal = ({ isOpen, onClose, imgUrl, buttonLink, link }) => {
+// Define the interface for ModalData
+interface ModalData {
+  imgUrl: string;
+  linkToOpen?: string;
+}
+
+// Define the interface for the props
+interface ImgModalProps {
+  children: (openModal: (imgUrl: string, linkToOpen?: string) => void) => ReactNode;
+}
+
+const ImgModal: React.FC<ImgModalProps> = ({ children }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState<ModalData>({
+    imgUrl: '',
+    linkToOpen: ''
+  });
+
+  const handleImageClick = (imgUrl: string, linkToOpen?: string) => {
+    setModalData({
+      imgUrl: imgUrl,
+      linkToOpen: linkToOpen ?? ''
+    });
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   useEffect(() => {
-    const handleKeyDown = (event) => {
+    const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
-        onClose();
+        closeModal();
       }
     };
 
@@ -13,33 +42,37 @@ const ImgModal = ({ isOpen, onClose, imgUrl, buttonLink, link }) => {
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [onClose]);
+  }, []);
 
-  const closeModal = (e) => {
+  const handleCloseModal = (e: MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
-    onClose();
+    closeModal();
   };
 
   return (
     <>
-      {isOpen && (
+      {children(handleImageClick)}
+      {showModal && (
         <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
           <div className="relative w-full h-full max-w-[90%] max-h-[90%]">
             <div
               className="absolute top-0 right-0 text-[var(--color-text-both)] text-6xl
               flex flex-col items-center"
             >
-              <button onClick={closeModal}>
+              <button onClick={handleCloseModal}>
                 &times;
               </button>
               {
-                buttonLink && <a href={link} target='_blank'>Ver</a>
+                modalData.linkToOpen &&
+                <a href={modalData.linkToOpen} target='_blank' rel='noopener noreferrer'>
+                  <span className="icon-[ph--eye]" />
+                </a>
               }
             </div>
             <div className="flex justify-center items-center h-full">
               <img
                 className="max-w-full max-h-full object-contain border-2 border-[var(--color-border)]"
-                src={imgUrl}
+                src={modalData.imgUrl}
                 alt="Imagen Ampliada"
               />
             </div>
