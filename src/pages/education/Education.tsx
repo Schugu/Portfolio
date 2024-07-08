3172278c864eed4bb521c1b977d4b31e9332f2e5
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-08 17:25:47 -0300

    Add: Implementación de la modal

--- src/pages/education/Education.tsx
+++ src/pages/education/Education.tsx
@@ -1,6 +1,5 @@
 import SectionCard from "@/components/SectionCard.tsx";
 import { useProfile } from "@/context/ProfileContext.tsx";
-import { useState } from "react";
 import { useTranslation } from 'react-i18next';
 import ImgModal from "@/components/ImgModal.tsx";
 
@@ -8,55 +7,37 @@ export default function Education() {
   const { dataProfile, loading } = useProfile();
   const { t } = useTranslation('global');
 
-  const [showModal, setShowModal] = useState(false);
-  const [modalData, setModalData] = useState({
-    imgUrl: '',
-    linkToOpen: ''
-  });
-
-  const handleImageClick = (imgUrl, linkToOpen) => {
-    setModalData({
-      imgUrl: imgUrl,
-      linkToOpen: linkToOpen
-    });
-    setShowModal(true);
-  };
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
   return (
     <SectionCard sectionTitle={t("global:education.section")}>
       {loading && <p>Cargando...</p>}
       {!loading && !dataProfile && <p>No hay información disponible.</p>}
 
-      {!loading && dataProfile?.certificates &&
-        <article className="w-full gap-[4vw] grid grid-cols-2 sm:grid-cols-4"
-        >
-          {
-            dataProfile.certificates.map((certificate, index) => (
-              <div key={index}
-                onClick={() =>
-                  handleImageClick(
-                    `/profile/education/${index + 1}.jpg`, 
-                    certificate
-                  )
-                }
-                className="w-full">
-                <img
-                  className='transform  hover:scale-110 transition duration-300
-                border-2 border-[var(--color-border-complementary)] rounded overflow-hidden'
-                  src={`/profile/education/${index + 1}.jpg`}
-                  alt={`Screenshot ${index + 1}`}
-                />
-              </div>
-            ))
-          }
-          <ImgModal isOpen={showModal} onClose={closeModal} imgUrl={modalData.imgUrl} buttonLink={true} link={modalData.linkToOpen} />
-
-        </article>
-      }
+      {!loading && dataProfile?.certificates && (
+        
+        <ImgModal>
+          {handleImageClick => (
+            <article className="w-full gap-[4vw] grid grid-cols-2 sm:grid-cols-4">
+              {dataProfile.certificates?.map((certificate, index) => (
+                <div key={index}
+                  onClick={() =>
+                    handleImageClick(
+                      `/profile/education/${index + 1}.jpg`,
+                      certificate
+                    )
+                  }
+                  className="w-full">
+                  <img
+                    className='transform hover:scale-110 transition duration-300 cursor-pointer
+                    border-2 border-[var(--color-border-complementary)] rounded overflow-hidden'
+                    src={`/profile/education/${index + 1}.jpg`}
+                    alt={`Screenshot ${index + 1}`}
+                  />
+                </div>
+              ))}
+            </article>
+          )}
+        </ImgModal>
+      )}
     </SectionCard>
-  )
+  );
 }
