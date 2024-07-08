7c65d6b56ca421a86ffa4858816a6dd378458798
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-08 11:36:47 -0300

    Refactor: Medidas y estilo

--- src/components/ImgModal.tsx
+++ src/components/ImgModal.tsx
@@ -24,9 +24,9 @@ const ImgModal = ({ isOpen, onClose, imgUrl }) => {
     <>
       {isOpen && (
         <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
-          <div className="relative w-full h-full max-w-screen-xl max-h-screen-90 overflow-hidden">
+          <div className="relative w-full h-full max-w-[90%] max-h-[90%]">
             <button
-              className="absolute top-0 right-0 m-4 text-white text-2xl"
+              className="absolute top-0 right-0 text-[var(--color-text-both)] text-6xl"
               onClick={closeModal}
             >
               &times;
