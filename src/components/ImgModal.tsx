8062b6e38f85bbafcf25daa1d2028b29c5fee2af
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-08 17:49:52 -0300

    Refactor: Forma de cerrar la modal y estilos

--- src/components/ImgModal.tsx
+++ src/components/ImgModal.tsx
@@ -49,29 +49,39 @@ const ImgModal: React.FC<ImgModalProps> = ({ children }) => {
     closeModal();
   };
 
+  const handleOutsideClick = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  };
+
   return (
     <>
       {children(handleImageClick)}
       {showModal && (
-        <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
-          <div className="relative w-full h-full max-w-[90%] max-h-[90%]">
+        <div
+          className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-90"
+          
+        >
+          <div className="relative w-full h-full max-w-[90%] max-h-[90%]" >
             <div
-              className="absolute top-0 right-0 text-[var(--color-text-both)] text-6xl
+              className="absolute top-0 right-0 text-[var(--color-primary-extraLight)] text-2xl md:text-6xl
               flex flex-col items-center"
             >
-              <button onClick={handleCloseModal}>
-                &times;
+              <button onClick={handleCloseModal} className='hover:text-[var(--color-primary)]'>
+                <span className="icon-[ion--close-circled]" />
               </button>
               {
                 modalData.linkToOpen &&
-                <a href={modalData.linkToOpen} target='_blank' rel='noopener noreferrer'>
-                  <span className="icon-[ph--eye]" />
+                <a href={modalData.linkToOpen} className='hover:text-[var(--color-primary)]'
+                target='_blank' rel='noopener noreferrer'>
+                  <span className="icon-[iconoir--internet]" />
                 </a>
               }
             </div>
-            <div className="flex justify-center items-center h-full">
+            <div className="flex justify-center items-center h-full" onClick={handleOutsideClick}>
               <img
-                className="max-w-full max-h-full object-contain border-2 border-[var(--color-border)]"
+                className="select-none max-w-full max-h-full object-contain border-2 border-[var(--color-border)]"
                 src={modalData.imgUrl}
                 alt="Imagen Ampliada"
               />
