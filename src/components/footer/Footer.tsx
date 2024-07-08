7dbc18561c9e02f4b28d4542ebdcb14d02363a20
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-08 18:34:41 -0300

    Refactor: Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,19 +5,23 @@ export default function Footer() {
   const { t } = useTranslation('global');
 
   return (
-    <footer className="w-full min-h-10 p-2 gap-2 flex flex-wrap items-center justify-center
+    <footer className="w-full min-h-10 p-2 gap-2 flex flex-wrap items-center justify-end  
     bg-[var(--color-background-dark)] text-[var(--color-text)]">
-      <p className="text-center">{t("global:footer.template")} Leandro Daniel Schugurensky</p>
+      <div className='flex gap-2 items-center text-lg justify-center'>
+        <p className="text-center">{t("global:footer.template1")}</p>
+        <span className="w-6 h-6 text-red-500 icon-[gravity-ui--card-heart]" />
+        <p className='text-center'>{t("global:footer.template2")}</p>
+      </div>
       <Tooltip text='Github'>
         <a href="https://github.com/Schugu" target="_blank"
           className="flex justify-center items-center hover:text-[var(--color-primary)]">
-          <span className="w-5 h-5 icon-[mdi--github]" />
+          <span className="w-6 h-6 icon-[mdi--github]" />
         </a>
       </Tooltip>
       <Tooltip text='Linkedin'>
         <a href="https://www.linkedin.com/in/leandrodanielschugurensky/" target="_blank"
           className="flex justify-center items-center hover:text-[var(--color-primary)]">
-          <span className="w-5 h-5 icon-[mdi--linkedin]" />
+          <span className="w-6 h-6 icon-[mdi--linkedin]" />
         </a>
       </Tooltip>
     </footer>
