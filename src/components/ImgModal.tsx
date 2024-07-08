ddb31c4bd815a05a1debec725135926598d458a6
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-08 12:00:55 -0300

    Add: Opción de poner un botón de redirección

--- src/components/ImgModal.tsx
+++ src/components/ImgModal.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-const ImgModal = ({ isOpen, onClose, imgUrl }) => {
+const ImgModal = ({ isOpen, onClose, imgUrl, buttonLink, link }) => {
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.key === 'Escape') {
@@ -25,12 +25,17 @@ const ImgModal = ({ isOpen, onClose, imgUrl }) => {
       {isOpen && (
         <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
           <div className="relative w-full h-full max-w-[90%] max-h-[90%]">
-            <button
-              className="absolute top-0 right-0 text-[var(--color-text-both)] text-6xl"
-              onClick={closeModal}
+            <div
+              className="absolute top-0 right-0 text-[var(--color-text-both)] text-6xl
+              flex flex-col items-center"
             >
-              &times;
-            </button>
+              <button onClick={closeModal}>
+                &times;
+              </button>
+              {
+                buttonLink && <a href={link} target='_blank'>Ver</a>
+              }
+            </div>
             <div className="flex justify-center items-center h-full">
               <img
                 className="max-w-full max-h-full object-contain border-2 border-[var(--color-border)]"
