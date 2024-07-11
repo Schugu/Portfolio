166838cefb1e52fa29e5b337cc59fbc289f6c318
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-11 14:15:31 -0300

    Add: tabIndex

--- src/pages/home/ContImg.tsx
+++ src/pages/home/ContImg.tsx
@@ -1,16 +1,17 @@
 interface ContImgProps {
   src: string;
-  shape: "square" | "circle";
+  shape?: "square" | "circle";
   alt?: string;
+  tabIndex: number;
 }
 
-export default function ContImg({ src, shape }: ContImgProps) {
+export default function ContImg({ src, shape = "circle", alt, tabIndex }: ContImgProps) {
   return (
     <div className={`w-48 h-48 sm:w-80 sm:h-80
       md:w-full md:h-full 
       ${shape === "circle" ? "rounded-full overflow-hidden" : "rounded"}
       border-2 border-[var(--color-primary)] bg-gradient-to-b from-[var(--color-secondary)] to-[var(--color-primary)]`}>
-      <img className="w-full h-full object-cover object-center" src={src} alt="Profile Picture" />
+      <img className="w-full h-full object-cover object-center" src={src} alt={alt} tabIndex={tabIndex} />
     </div>
   );
 }
