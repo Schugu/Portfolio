cacaa1c8e712eab15965bda72907c7ec112508ef
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-11 15:20:01 -0300

    Add: tabIndex

--- src/pages/cv/Cv.tsx
+++ src/pages/cv/Cv.tsx
@@ -10,12 +10,12 @@ export default function Cv() {
   return (
     <SectionCard sectionTitle={t("global:cv.section")}>
       {loading && <SkeletonUniversal />}
-      {!loading && !dataProfile && <p className="text-3xl text-center">{t("global:infoNotAvailable")}</p>}
+      {!loading && !dataProfile && <p className="text-3xl text-center" tabIndex={3}>{t("global:infoNotAvailable")}</p>}
 
       {!loading && dataProfile?.linksSocialNetworks?.CV &&
         <article className="w-full h-full flex flex-col gap-2 p-2">
           <div className="pdf-container" style={{ width: '100%', height: '100vh' }}>
-            <embed src={t("profile:linksSocialNetworks.CV")} width="100%" height="100%" type="application/pdf" />
+            <embed src={t("profile:linksSocialNetworks.CV")} width="100%" height="100%" type="application/pdf" tabIndex={4}/>
           </div>
         </article>
       }
