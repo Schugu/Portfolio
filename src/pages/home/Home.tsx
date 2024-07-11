fd254939344f702a9c381d77899f5e22bad588d4
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-11 14:15:13 -0300

    Add: tabIndex

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -22,20 +22,20 @@ export default function Home() {
 
           <div className="w-full flex flex-col items-center gap-8">
             <p
-              className="text-center text-2xl md:text-5xl text-[var(--color-primary)]" tabIndex={7}>
+              className="text-center text-2xl md:text-5xl text-[var(--color-primary)]" tabIndex={3}>
               {dataProfile.fullname}
             </p>
 
             <section className="w-full flex gap-5 flex-wrap justify-center items-center text-base md:text-xl">
               {dataProfile.titles?.map((_, index) => (
-                <Subtitle string={t(`profile:titles.${index}`)} tabIndex={9} key={index} />
+                <Subtitle string={t(`profile:titles.${index}`)} tabIndex={4} key={index} />
               ))}
               {dataProfile.years &&
-                <Subtitle string={t("profile:years")} tabIndex={9} />
+                <Subtitle string={t("profile:years")} tabIndex={5} />
               }
               {dataProfile.nationality &&
                 <Tooltip text={dataProfile.nationality}>
-                  <Flag nationality={dataProfile.nationality?.toLowerCase()} tabIndex={10} />
+                  <Flag nationality={dataProfile.nationality?.toLowerCase()} tabIndex={6} />
                 </Tooltip>
               }
             </section>
@@ -59,7 +59,7 @@ export default function Home() {
                 <section className="flex flex-wrap gap-0.5 md:gap-4 items-center md:items-start">
                   {Object.entries(dataProfile.linksSocialNetworks).map(([network, link], index) => (
                     <Tooltip text={network} key={network + index}>
-                      <SocialNetworkIcon socialNetwork={network} link={link} tabIndex={10 + index} />
+                      <SocialNetworkIcon socialNetwork={network} link={link} tabIndex={8} />
                     </Tooltip>
                   ))}
                 </section>
@@ -67,19 +67,19 @@ export default function Home() {
 
               {dataProfile.skills &&
                 <section className="w-full flex flex-col gap-2 items-center md:items-start">
-                  <p className="text-[var(--color-secondary)] text-base md:text-2xl text-center" tabIndex={50}>
+                  <p className="text-[var(--color-secondary)] text-base md:text-2xl text-center" tabIndex={9}>
                     {t("global:homePage.skills.title")}
                   </p>
 
                   <article className="w-4/6 md:w-full flex flex-col items-start gap-6">
                     {Object.entries(dataProfile.skills).map(([titleStack, stack], index) => (
                       <div className="flex items-center flex-wrap gap-x-4 gap-y-1" key={titleStack + index}>
-                        <p className="text-[var(--color-primary)] text-md md:text-xl" tabIndex={51}>
+                        <p className="text-[var(--color-primary)] text-md md:text-xl" tabIndex={10}>
                           {t(`global:homePage.skills.${titleStack}`)}:
                         </p>
                         {stack.map((tech, stackindex) => (
                           <Tooltip text={tech} key={titleStack + tech + stackindex}>
-                            <IconosTechs tech={tech} tabIndex={51 + stackindex} />
+                            <IconosTechs tech={tech} tabIndex={10}/>
                           </Tooltip>
                         ))}
                       </div>
@@ -93,18 +93,18 @@ export default function Home() {
           <div className="w-full flex flex-col gap-8 justify-center">
             {dataProfile.aboutMe &&
               <section className="w-full flex flex-col gap-2">
-                <p className="text-[var(--color-secondary)] text-base md:text-2xl text-center">{t("global:homePage.aboutMe")}</p>
+                <p className="text-[var(--color-secondary)] text-base md:text-2xl text-center" tabIndex={11}>{t("global:homePage.aboutMe")}</p>
                 {dataProfile.aboutMe?.map((_, pIndex) => (
-                  <p className="indent-8 text-base md:text-xl text-left" key={pIndex}>{t(`profile:aboutMe.${pIndex}`)}</p>
+                  <p className="indent-8 text-base md:text-xl text-left" key={pIndex} tabIndex={12}>{t(`profile:aboutMe.${pIndex}`)}</p>
                 ))}
               </section>
             }
 
             {dataProfile.hobbies &&
               <section className="w-full flex flex-col gap-2">
-                <p className="text-[var(--color-secondary)] text-base md:text-2xl text-center">{t("global:homePage.hobbies")}</p>
+                <p className="text-[var(--color-secondary)] text-base md:text-2xl text-center" tabIndex={13}>{t("global:homePage.hobbies")}</p>
                 {dataProfile.hobbies?.map((_, pIndex) => (
-                  <p className="indent-8 text-base md:text-xl text-left" key={pIndex}>{t(`profile:hobbies.${pIndex}`)}</p>
+                  <p className="indent-8 text-base md:text-xl text-left" key={pIndex} tabIndex={14}>{t(`profile:hobbies.${pIndex}`)}</p>
                 ))}
               </section>
             }
