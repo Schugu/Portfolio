5189c0f1dbd2bb23a7ac76ebb6e75e5554a71c26
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-11 15:17:42 -0300

    Add: pulsar boton con enter y tabindex

--- src/pages/projects/SelectResolution.tsx
+++ src/pages/projects/SelectResolution.tsx
@@ -8,22 +8,28 @@ interface SelectResolutionProps {
   tabIndex: number;
 }
 
-export default function SelectResolution(props: SelectResolutionProps) {
-  const { children, onClick, device, resolution, tabIndex } = props;
+export default function SelectResolution({ children, onClick, device, resolution, tabIndex }: SelectResolutionProps) {
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter' && onClick) {
+      onClick();
+    }
+  };
 
   return (
     <section
       className={`flex flex-col items-center justify-end p-2 
-      rounded border-2 cursor-pointer select-none
-      transition-colors duration-100 ease-in-out 
-      hover:border-[var(--color-primary)] hover:shadow-[var(--color-primary)] hover:bg-[var(--color-colorOpacity)] 
-      ${device === resolution
+        rounded border-2 cursor-pointer select-none
+        transition-colors duration-100 ease-in-out 
+        hover:border-[var(--color-primary)] hover:shadow-[var(--color-primary)] hover:bg-[var(--color-colorOpacity)] 
+        ${device === resolution
           ? 'border-[var(--color-primary)] shadow-[var(--color-primary)] bg-[var(--color-colorOpacity)]'
-          : 'border-[var(--color-colorOpacity)] '}
-      `}
+          : 'border-[var(--color-colorOpacity)] '}`
+      }
       onClick={onClick}
+      onKeyDown={handleKeyPress}
       tabIndex={tabIndex}
       aria-label={device}
+      role="button"
     >
       {children}
     </section>
