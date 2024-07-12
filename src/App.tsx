135ba609ad294cea69ffa6f96af0bad905caf841
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-12 12:50:43 -0300

    Refactor: Variables de color

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       <ThemeProvider>
         <ProfileProvider>
           <I18nextProvider i18n={i18next}>
-            <main className='flex flex-col min-h-screen bg-[var(--color-background-light)] text-[var(--color-text)]'>
+            <main className='flex flex-col min-h-screen bg-[var(--color-background-second)] text-[var(--color-text-first)]'>
               <Navbar />
 
               <section className='flex-grow'>
